c7185fade4e22cd66a4f22c2a395e0548d3cdb74
Ana Júlia <ajulia.silva1@gmail.com>
2024-10-09 21:26:15 -0300

    Create exercicio2.js

---
+++ 09-10/exercicio2.js
@@ -0,0 +1,30 @@
+const readline = require("readline-sync");
+
+/** 
+ * Solicitar a idade e o sexo da pessoa.
+ * Se for maior ou igual a 18 anos, verificar se é do sexo masculino.
+ * Se for do sexo masculino, informar "Bem-vindo ao exército brasileiro";
+ * Se for do sexo feminino, perguntar "Desejar se alistar ao exército? ".
+ * Se sim, informar "Bem-vindo ao exército brasileiro".
+ * Se não, informar "Obrigado."
+*/
+
+const nome = readline.question("Qual seu nome?\n");
+
+const idade = readline.questionInt("Qual a sua idade?\n");
+// MAIOR DE IDADE
+if (idade >= 18) {
+    const sexo = readline.question("Qual o seu sexo? M - Masculino | F - Feminino \n")
+    if (sexo.toUpperCase() == 'M') {
+        console.log("Bem-vindo ao exército brasileiro");
+    } else if (sexo.toUpperCase() == 'F') {
+        const resposta = readline.question("Desejar se alistar ao exercito? S - SIM | N - Nao\n");
+        if (resposta.toUpperCase() == 'S') {
+            console.log("Bem-vindo ao exército brasileiro");
+        } else {
+            console.log("Obrigado.");
+        }
+    }
+} else {
+    console.log("Voce ainda e menor de idade. ");
+}
