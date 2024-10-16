b34af131a7e540685e569c9f339fcda404de9ca8
Ana Júlia <ajulia.silva1@gmail.com>
2024-10-16 20:39:35 -0300

    Create index.js

---
+++ 16-10/index.js
@@ -0,0 +1,17 @@
+const readline = require('readline-sync')
+
+// Laços de Repetição
+// FOR --- Para
+
+
+
+for(let i = 0; i < 5; i++) {
+    console.log(i);
+}
+
+
+
+// WHILE --- Enquanto
+// DO WHILE -- Faça enquanto
+
+//npm install readline-sync
