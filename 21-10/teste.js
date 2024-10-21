8312b0226069d63b6eef0c31e3dc409d7842807d
Ana Júlia <ajulia.silva1@gmail.com>
2024-10-21 20:58:05 -0300

    Create teste.js

---
+++ 21-10/teste.js
@@ -0,0 +1,23 @@
+// let nome = "Ana"; // String
+// let idade = 30; // Number
+// let estaAprovado = true; // Boolean
+// let carro = { marca: "Honda", modelo: "Civic" }; // Object
+
+// console.log("Olá, " + nome + "! Você tem " + idade + " anos.");
+// console.log("Está aprovado? " + estaAprovado);
+// console.log("Seu carro é um " + carro.marca + " " + carro.modelo);
+// // ou 
+// console.log(`Seu carro é um ${carro.marca} ${carro.modelo}`);
+// // Convertendo para string
+// let idadeComoString = idade.toString();
+// console.log(typeof idadeComoString); // "string"
+
+
+let nome = "Ana"; // String
+let idade = 30; // Number
+let estaAprovado = true; // Boolean
+let carro = { marca: "Honda", modelo: "Civic" }; // Object
+
+console.log("Olá, " + nome + "! Você tem " + idade + " anos.");
+console.log("Está aprovado? " + estaAprovado);
+console.log("Seu carro é um " + carro.marca + " " + carro.modelo);
