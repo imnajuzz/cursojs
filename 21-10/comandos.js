237251a5aefa81f910892457dafd36c54904a3f8
Ana Júlia <ajulia.silva1@gmail.com>
2024-10-21 20:58:35 -0300

    Create comandos.js

---
+++ 21-10/comandos.js
@@ -0,0 +1,13 @@
+let frutas = ["maçã", "banana", "laranja"];
+frutas.push("uva");
+frutas.unshift("melão");
+console.log(frutas, frutas.length);
+frutas.pop();
+frutas.shift();
+console.log(frutas, frutas.length);
+
+// Length - Tamanho / Quantidade de elementos
+// Push - Incluir no fim do Array
+// Unshift - Incluir no começo do Array
+// Pop - Remove o último elemento do Array
+// Shift - Remove o primeiro elemento do Array
