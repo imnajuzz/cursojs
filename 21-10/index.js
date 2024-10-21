1d8475dc3b5e28eda93d31691a7a4a95a817650a
Ana Júlia <ajulia.silva1@gmail.com>
2024-10-21 20:58:47 -0300

    Create index.js

---
+++ 21-10/index.js
@@ -0,0 +1,17 @@
+const readline = require("readline-sync");
+
+const notas = [];
+for (let i = 0; i < 4; i++) {
+    let nota = readline.questionFloat(`Qual a nota ${i}: `);
+    notas.push(nota);
+}
+console.log(notas);
+// let soma = 0;
+// for (let i = 0; i < notas.length; i++) {
+//     soma = soma + notas[i];
+// }
+// console.log (soma/notas.length);
+
+const somaReduce = notas.reduce(  (total, atual) => total + atual, 0);
+console.log(somaReduce);
+console.log(somaReduce/notas.length);
