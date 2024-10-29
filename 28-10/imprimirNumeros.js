4cd4ba3c8a29abf55feed714128c0a2d030568a1
Ana Júlia <ajulia.silva1@gmail.com>
2024-10-28 21:11:43 -0300

    Create imprimirNumeros.js

---
+++ 28-10/imprimirNumeros.js
@@ -0,0 +1,11 @@
+// Escreva um programa que imprima os números de 1 à 20.
+
+// Função para imprimir números de 1 a 20
+function imprimirNumeros() {
+    for (let i = 1; i <= 20; i++) {
+        console.log(i);
+    }
+}
+
+// Chama a função
+imprimirNumeros();
