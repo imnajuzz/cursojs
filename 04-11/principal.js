68425a86acca38be094eeecf29e3c401c3f3515c
Ana Júlia <ajulia.silva1@gmail.com>
2024-11-04 21:43:33 -0300

    Create principal.js

---
+++ 04-11/principal.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    
+    // // Javascript Padrão
+    // document.querySelector("button#btnAlerta").addEventListener('click', function() {
+    //     alert("Oi");
+    // });
+
+    $("#btnAlerta").on('click', function() {
+        alert("AGORA COM JQUERY");
+    });
+});
